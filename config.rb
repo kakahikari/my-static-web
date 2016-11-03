eac6d481ff4259a91f6d269fa30bc8d9743cc68a
kakahikari <kakahikari@gmail.com>
2016-11-03 10:51:19 +0800

    first commit

---
+++ config.rb
@@ -0,0 +1,23 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+Encoding.default_external = "UTF-8"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "src/stylesheet"
+sass_dir = "scss"
+images_dir = "src/images"
+javascripts_dir = "src/js"
+# fonts_dir = "Public/fonts"
+
+output_style = :compressed
+sourcemap = true
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
